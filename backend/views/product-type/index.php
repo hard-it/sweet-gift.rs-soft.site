904944cd43753c36571956f119465ede53510698
Рындин Михаил Анатолиевич <mryndin@stream.ru>
2019-11-17 12:48:27 +0300

    Custom action for ProductType grid.

--- backend/views/product-type/index.php
+++ backend/views/product-type/index.php
@@ -5,6 +5,7 @@ use yii\grid\GridView;
 use yii\widgets\Pjax;
 use kartik\tree\TreeViewInput;
 use common\models\ProductCategory;
+use common\models\ProductType;
 
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\ProductTypeSearch */
@@ -18,85 +19,90 @@ echo Html::beginTag('div', ['class' => 'product-type-index']);
 echo Html::tag('h1', Html::encode($this->title));
 
 ?>
-    <?php Pjax::begin(); ?>
-    <?php
-    echo $this->render('_search', ['model' => $searchModel]);
-    ?>
+<?php Pjax::begin(); ?>
+<?php
+echo $this->render('_search', ['model' => $searchModel]);
+?>
 
-    <?php
-    echo Html::beginTag('div', ['class' => 'box box-no-top-border']);
-    echo GridView::widget([
-        'dataProvider' => $dataProvider,
-        //'filterModel'  => $searchModel,
-        'tableOptions' => [
-            'class' => 'table table-bordered table-hover',
+<?php
+echo Html::beginTag('div', ['class' => 'box box-no-top-border']);
+echo GridView::widget([
+    'dataProvider' => $dataProvider,
+    //'filterModel'  => $searchModel,
+    'tableOptions' => [
+        'class' => 'table table-bordered table-hover',
+    ],
+    'columns'      => [
+        ['class' => 'yii\grid\SerialColumn'],
+        [
+            'attribute' => 'Code',
+            'label'     => Yii::t('app', 'Код'),
+            //'contentOptions' => ['style' => 'width:3%'],
         ],
-        'columns'      => [
-            ['class' => 'yii\grid\SerialColumn'],
-            [
-                'attribute'      => 'Code',
-                'label' => Yii::t('app','Код'),
-                //'contentOptions' => ['style' => 'width:3%'],
-            ],
-            [
-                'value'          => function ($model) {
+        [
+            'value'     => function ($model) {
 
-                    /* @var \common\models\ProductType $model */
-                    /* @var \common\models\ProductCategory $category */
-                    $category = $model->getCategory0()->one();
+                /* @var \common\models\ProductType $model */
+                /* @var \common\models\ProductCategory $category */
+                $category = $model->getCategory0()->one();
 
-                    return isset($category) ? $category->Name : '';
-                },
-                'label' => Yii::t('app','Категория'),
-                'attribute'      => 'Category',
-                //'contentOptions' => ['style' => 'width:25%'],
-                /*
-                'filter'         => TreeViewInput::widget(
-                    [
+                return isset($category) ? $category->Name : '';
+            },
+            'label'     => Yii::t('app', 'Категория'),
+            'attribute' => 'Category',
+            //'contentOptions' => ['style' => 'width:25%'],
+            /*
+            'filter'         => TreeViewInput::widget(
+                [
 
-                        'model'          => $searchModel,
-                        'attribute'      => 'Category',
-                        'value'          => $searchModel->Category, // preselected values
-                        'query'          => ProductCategory::find()->addOrderBy('root, lft, Name'),
-                        'headingOptions' => ['label' => 'Категории'],
-                        'rootOptions'    => ['label' => 'Все категории'],
-                        'fontAwesome'    => true,
-                        'asDropdown'     => true,
-                        'multiple'       => true,
-                        'options'        => [
-                            'disabled' => false,
-                        ],
-                    ]
-                ),*/
-            ],
-            [
-                'attribute'      => 'Name',
-                'label' => Yii::t('app','Наименование'),
-                //'contentOptions' => ['style' => 'min-width:50%'],
-            ],
-            [
-                'attribute'      => 'MinimalQuantity',
-                'label' => Yii::t('app','Мин. кол.'),
-                //'contentOptions' => ['style' => 'width:3%'],
-            ],
-            //'ShelfLife',
-            //'Measure',
-            [
-                'attribute'      => 'Cost',
-                'label' => Yii::t('app','Цена'),
-                //'contentOptions' => ['style' => 'width:3%'],
-            ],
-            //'Description:ntext',
-            //'Tags',
-            //'Keywords',
-            //'Images',
-
-            [
-                'class'          => 'yii\grid\ActionColumn',
-                //'contentOptions' => ['style' => 'width:3%'],
-            ],
+                    'model'          => $searchModel,
+                    'attribute'      => 'Category',
+                    'value'          => $searchModel->Category, // preselected values
+                    'query'          => ProductCategory::find()->addOrderBy('root, lft, Name'),
+                    'headingOptions' => ['label' => 'Категории'],
+                    'rootOptions'    => ['label' => 'Все категории'],
+                    'fontAwesome'    => true,
+                    'asDropdown'     => true,
+                    'multiple'       => true,
+                    'options'        => [
+                        'disabled' => false,
+                    ],
+                ]
+            ),*/
         ],
-    ]);
+        [
+            'attribute' => 'Name',
+            'label'     => Yii::t('app', 'Наименование'),
+            //'contentOptions' => ['style' => 'min-width:50%'],
+        ],
+        [
+            'attribute' => 'MinimalQuantity',
+            'label'     => Yii::t('app', 'Мин. кол.'),
+            //'contentOptions' => ['style' => 'width:3%'],
+        ],
+        //'ShelfLife',
+        //'Measure',
+        [
+            'attribute' => 'Cost',
+            'label'     => Yii::t('app', 'Цена'),
+            //'contentOptions' => ['style' => 'width:3%'],
+        ],
+        [
+            'format' => 'raw',
+            'value'  => function ($model) {
+
+                /* @var ProductType $model */
+                $update = Html::tag('i', '', ['class' => 'fa fa-pencil gridview-update-button', 'data-key' => $model->Id]);
+
+                $delete = Html::tag('i', '', ['class' => 'fa fa-trash gridview-delete-button', 'data-key' => $model->Id]);
+
+                $div = Html::tag('div', $update.$delete, ['class' => 'gridview-buttons']);
+                return $div;
+            }
+            ,
+        ],
+    ],
+]);
 
 echo Html::beginTag('div', ['class' => 'row']);
 
@@ -105,7 +111,7 @@ echo Html::beginTag('div', ['class' => 'col-12 col-md-3']);
 echo Html::a(Yii::t('app', 'Создать новый тип'),
     ['create'],
     [
-        'class' => 'btn btn-lg bg-olive fa fa-plus btn-block btn-flat'
+        'class' => 'btn btn-lg bg-olive fa fa-plus btn-block btn-flat',
     ]
 );
 echo Html::endTag('div');
@@ -114,6 +120,6 @@ echo Html::endTag('div');
 
 echo Html::endTag('div');
 
-    Pjax::end();
+Pjax::end();
 
-    echo Html::endTag('div');
+echo Html::endTag('div');
