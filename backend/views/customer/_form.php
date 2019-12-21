1ad198e668ae2cdcda56f90f5327207ac6ecdf41
Michael Ryndin <softarts@mail.ru>
2019-12-21 11:48:13 +0300

    Fix form previous Button

--- backend/views/customer/_form.php
+++ backend/views/customer/_form.php
@@ -1,29 +1,54 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use kartik\form\ActiveForm;
+use borales\extensions\phoneInput\PhoneInput;
+use backend\helpers\js\grids\ButtonHelper;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Customer */
 /* @var $form yii\widgets\ActiveForm */
-?>
 
-<div class="customer-form">
+$buttonHelper = new ButtonHelper($this);
 
-    <?php $form = ActiveForm::begin(); ?>
+echo Html::beginTag('div', ['class' => 'customer-form']);
 
-    <?= $form->field($model, 'Phone')->textInput(['maxlength' => true]) ?>
+    $form = ActiveForm::begin();
 
-    <?= $form->field($model, 'Firstname')->textInput(['maxlength' => true]) ?>
+    echo Html::beginTag('div', ['class' => 'row']);
 
-    <?= $form->field($model, 'Lastname')->textInput(['maxlength' => true]) ?>
+        echo Html::beginTag('div', ['class' => 'col-lg-4 col-xs-12']);
+            echo $form->field($model, 'Phone')->widget(PhoneInput::className(), [
+                'jsOptions' => [
+                    'preferredCountries' => ['ru', 'pl', 'ua'],
+                ]
+            ]);
+        echo Html::endTag('div');
 
-    <?= $form->field($model, 'User')->textInput() ?>
+        echo Html::beginTag('div', ['class' => 'col-lg-4 col-xs-12']);
+            echo $form->field($model, 'Firstname');
+        echo Html::endTag('div');
 
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
-    </div>
+        echo Html::beginTag('div', ['class' => 'col-lg-4 col-xs-12']);
+            echo $form->field($model, 'Lastname');
+        echo Html::endTag('div');
 
-    <?php ActiveForm::end(); ?>
+    echo Html::endTag('div');
 
-</div>
+    echo Html::beginTag('div', ['class' => 'form-group']);
+        echo Html::beginTag('div', ['class' => 'row']);
+            echo Html::beginTag('div', ['class' => 'col-3 col-md-3 col-lg-3 col-sm-4 col-xs-6']);
+                echo Html::submitButton(Yii::t('app', 'Сохранить'), ['class' => 'btn btn-lg btn-primary btn-success fa fa-save btn-block btn-flat']);
+            echo Html::endTag('div');
+            echo Html::beginTag('div', ['class' => 'col-3 col-md-3 col-lg-3 col-sm-4 col-xs-6']);
+                echo Html::submitButton(Yii::t('app', 'Назад'), ['id'=>'previous-button', 'class' => 'btn btn-lg btn-primary fa fa-undo btn-block btn-flat btn-back']);
+            echo Html::endTag('div');
+        echo Html::endTag('div');
+    echo Html::endTag('div');
+
+
+ActiveForm::end();
+
+echo Html::endTag('div');
+
+$buttonHelper->registerPreviousMoveScript('previous-button');
