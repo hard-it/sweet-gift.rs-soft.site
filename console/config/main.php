9ef695fd083613c3704e70ccd51de5efb8eec361
Рындин Михаил Анатолиевич <mryndin@stream.ru>
2020-03-10 17:15:30 +0300

    Add migration

--- console/config/main.php
+++ console/config/main.php
@@ -21,6 +21,25 @@ return [
             'namespace' => 'common\fixtures',
           ],
     ],
+    'modules'             => [
+        'treemanager' => [
+            'class'           => '\kartik\tree\Module',
+            // other module settings, refer detailed documentation
+            'dataStructure'   => [
+                'keyAttribute'  => 'Id',
+                'nameAttribute' => 'Name',
+            ],
+            'treeEncryptSalt' => '5d95646cbc48343c9531edc114989e60feee568d9b0c3cc3c4a0e9e60ead6564',
+        ],
+        'gridview'    => [
+            'class' => '\kartik\grid\Module'
+            // enter optional module parameters below - only if you need to
+            // use your own export download action or custom translation
+            // message source
+            // 'downloadAction' => 'gridview/export/download',
+            // 'i18n' => []
+        ],
+    ],
     'components' => [
         'log' => [
             'targets' => [
